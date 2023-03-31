23c715b6e4d34aae08bfa707eb1994232ecc6688
André Teixeira Silva <64730896+theandreteixeira@users.noreply.github.com>
2023-03-31 17:55:50 -0300

    v

--- src/pages/description/Description.jsx
+++ src/pages/description/Description.jsx
@@ -20,7 +20,8 @@ import {
     DrawerCloseButton,
     useDisclosure,
     Input,
-    Button
+    Button,
+    Tabs, TabList, Tab, TabPanels, TabPanel
   } from '@chakra-ui/react'
   import React from "react";
 
@@ -146,43 +147,25 @@ export const Description = () => {
             </DrawerFooter>
           </DrawerContent>
         </Drawer>
+        <Tabs>
+        <TabList>
+          <Tab>DESCRIÇÃO</Tab>
+          <Tab>Two</Tab>
+          <Tab>Three</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <p>one!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>three!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
         </>
     );
 };
-
-
-function DrawerExample() {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const btnRef = React.useRef()
-
-    return (
-      <>
-        <Button ref={btnRef} colorScheme='teal' onClick={onOpen}>
-          Open
-        </Button>
-        <Drawer
-          isOpen={isOpen}
-          placement='right'
-          onClose={onClose}
-          finalFocusRef={btnRef}
-        >
-          <DrawerOverlay />
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader>Create your account</DrawerHeader>
-
-            <DrawerBody>
-              <Input placeholder='Type here...' />
-            </DrawerBody>
-
-            <DrawerFooter>
-              <Button variant='outline' mr={3} onClick={onClose}>
-                Cancel
-              </Button>
-              <Button colorScheme='blue'>Save</Button>
-            </DrawerFooter>
-          </DrawerContent>
-        </Drawer>
-      </>
-    )
-  }
