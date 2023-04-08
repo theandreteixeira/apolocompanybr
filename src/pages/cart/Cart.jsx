431c38b3449377d609acd291e5914de200200118
André Teixeira Silva <64730896+theandreteixeira@users.noreply.github.com>
2023-04-08 16:31:10 -0300

    center

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -34,7 +34,7 @@ export const EmptyCart = () => {
     return (
         <>
        <Flex align="center" justifyContent="center">
-        <Box position='relative' p={'20px'}>
+        <Box align="center" position='relative' p={'20px'}>
                 <Text fontSize={'25'} fontWeight={'extrabold'}>SUA SACOLA ESTÁ VAZIA</Text>
                 <Text fontSize={'20px'}>Nenhum item adicionado a sacola ainda.</Text>
         </Box>
